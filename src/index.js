ceb8096bb81e10fe88bd7f63ad494e82cafe1be8
NadiiaTkalych <nadin.tkalich@gmail.com>
2022-02-07 17:01:59 +0200

    add 3 script

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const photos = document.querySelectorAll('img');
+
+////////1
+let bigsize = "300"; 
+let smallsize = "150";
+
+function changeSizeImage(photo) {
+if(photo.height == bigsize) photo.height = smallsize;
+else photo.height = bigsize;
+}
+
+// ////////2
+// for(let photo of photos) {
+//     photo.addEventListener('click', () => {
+//         let width = Math.floor(photo.naturalWidth * 0.5),
+//             height = Math.floor(photo.naturalHeight * 0.5);
+//         photo.style.width = width;
+//         photo.style.height = height;
+//     })
+// }
+
+// ///////3
+// for(let photo of photos) {
+//     photo.addEventListener('click', () => {
+//         photo.style.zoom = +photo.style.zoom * 2;
+//     })
+// };
